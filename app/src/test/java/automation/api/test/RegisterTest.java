8fde18827d0b76301d8115cb858a4acaa611ae43
Ana Paula <anapaula.martins17@hotmail.com>
2022-06-12 17:39:38 -0300

    Login missing password scenario

--- app/src/test/java/automation/api/test/RegisterTest.java
+++ app/src/test/java/automation/api/test/RegisterTest.java
@@ -1,16 +1,29 @@
 package automation.api.test;
 
 import automation.api.domain.User;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.is;
 
 public class RegisterTest extends BaseTest {
 
     private static final String REGISTER_USER_ENDPOINT = "/register";
+    private static final String LOGIN_USER_ENDPOINT = "/login";
+
+    @BeforeClass
+    public static void setupRegister() {
+        RestAssured.responseSpecification = new ResponseSpecBuilder().
+                expectStatusCode(HttpStatus.SC_BAD_REQUEST).
+                build();
+
+    }
 
     @Test
     public void testMissingPassword() {
@@ -20,9 +33,22 @@ public class RegisterTest extends BaseTest {
         given().
                 body(user).
         when().
-                post(REGISTER_USER_ENDPOINT).
+                post(LOGIN_USER_ENDPOINT).
         then().
-                statusCode(HttpStatus.SC_BAD_REQUEST).
+                body("error", is("Missing password"));
+
+    }
+
+    @Test
+    public void testLoginMissingPassword() {
+        User user = new User();
+        user.setEmail("sydney@fife");
+
+        given().
+                body(user).
+                when().
+                post(REGISTER_USER_ENDPOINT).
+                then().
                 body("error", is("Missing password"));
 
     }
